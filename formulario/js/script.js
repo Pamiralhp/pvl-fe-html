049835f942e8ea4affa0ee0b2d1993c1944fa8a4
Pavel_Bladimir <pamiralhp@gmail.com>
2023-06-08 17:45:21 +0200

    GDRP-Error

--- formulario/js/script.js
+++ formulario/js/script.js
@@ -1,63 +1,84 @@
-let errorNombre = errorEmail = errorTel = true;
+let NombreError = EmailError = TelfError = dninieError = GRPDError = true;
+
+function mostrarError(id, txt) {
+  document.getElementById(id).innerHTML = txt;
+}
 
 function validarNombre() {
-    var nombre = document.formulario.nombre.value;
-    console.log(nombre);
-    if (nombre === ""){
-        document.getElementById("errorNombre").innerHTML="Error: Campo vacio";
+  var nombre = document.formulario.nombre.value;
+  if (nombre === "") {
+    mostrarError("errorNombre", "ERROR: Campo vacio");
+  } else {
+    mostrarError("errorNombre", "");
+
+    var regex = /^[a-zA-Z\s]+$/;
+    if (regex.test(nombre) === false) {
+      mostrarError("errorNombre", "ERROR: Formato inválido");
+    } else {
+      mostrarError("errorNombre", "");
+      NombreError = false;
     }
-    else{
-        document.getElementById("errorNombre").innerHTML="";
-        var regex= /^[A-Za-z\s]+$/;
-        if (regex.test(nombre) === false) {
-            document.getElementById("errorNombre").innerHTML="Error: Formato invalido";
-        }
-        else{
-            document.getElementById("errorNombre").innerHTML="";
-            errorNombre = false;
-        }
-    }
+  }
 }
+
 function validarEmail() {
-    var email = document.formulario.email.value;
-    console.log(email);
-    if (email === ""){
-        document.getElementById("errorEmail").innerHTML="Error: Campo vacio";
+  var email = document.formulario.email.value;
+  if (email === "") {
+    document.getElementById("errorEmail").innerHTML = "ERROR: email  vacio";
+  } else {
+    document.getElementById("errorEmail").innerHTML = "";
+    var regex = /^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$/;
+    if (regex.test(email) === false) {
+      mostrarError("errorEmail", "ERROR: Formato email inválido");
+    } else {
+      mostrarError("errorEmail", "");
+      EmailError = false;
     }
-    else{
-        document.getElementById("errorEmail").innerHTML="";
-        var regex= /^[\w\-\.]+@([\w-]+\.)+[\w]{2,4}$/;
-        if (regex.test(email) === false) {
-            document.getElementById("errorEmail").innerHTML="Error: Formato invalido";
-        }
-        else{
-            document.getElementById("errorEmail").innerHTML="";
-            errorEmail = false;
-        }
-    }
+  }
 }
-function validarTel() {
-    var tel = document.formulario.tel.value;
-    console.log(tel);
-    if (tel === ""){
-        document.getElementById("errorTel").innerHTML="Error: Campo vacio";
-    }
-    else{
-        document.getElementById("errorTel").innerHTML="";
-        var regex= /^[6789]\d{8}$/;
-        if (regex.test(tel) === false) {
-            document.getElementById("errorTel").innerHTML="Error: Formato invalido";
-        }
-        else{
-            document.getElementById("errorTel").innerHTML="";
-            errorTel = false;
-        }
-    }
+
+function validarTelf() {
+  var telf = document.formulario.telf.value;
+  if (email === "") {
+    mostrarError("errorTelf", "ERROR: Telf  vacio");
+  } else {
+    mostrarError("errorTelf", "");
+    var regex = /^[6789]\d{8}/;
+
+    !regex.test(telf)
+      ? mostrarError("errorTelf", "ERROR: Formato email inválido")
+      : mostrarError("errorTelf", "");
+
+    TelfError = false;
+  }
 }
-function validation() {
-    if (!errorNombre && !errorEmail && !errorTel) 
-        return true;
-        else return false;
-    
-    
+function validarGRPD() {
+  let grpd = document.formulario.grpd.checked;
+  !grpd
+    ? mostrarError("errorGRPD", "ERROR: GRPD debe estar seleccionado")
+    : ("errorGRPD", "");
+  {
+    GRPDError = false;
+  }
 }
+function validarDniNie() {
+    let DniNie = "";
+    DniNie = document.formulario.dninie.value;
+    var regex = /^[A-Z]?d{8}[A-Z]$/;
+    !dniNie
+      ? mostrarError("errordniNie", "ERROR: DniNie debe")
+      : ("errorDniNie", "");
+    {
+      dninieError = false;
+    }
+  }
+  function validation() {
+    if (!NombreError && !EmailError && !TelfError) return true;
+    else {
+      validarNombre();
+      validarEmail();
+      validarTelf();
+      validarGRPD();
+      return false;
+    }
+  }
